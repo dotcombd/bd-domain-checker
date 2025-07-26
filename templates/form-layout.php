f0e0f2bdee855486ce59226bf56ead7ab09125ab
dotcombd <sobujsfs@gmail.com>
2025-07-26 22:18:20 +0600

    Update form-layout.php

--- templates/form-layout.php
+++ templates/form-layout.php
@@ -1,28 +1,26 @@
-<?php
-// form-layout.php
-
-?>
-<!-- ডিজাইন শুরু -->
-<style>
-/* CSS কোড এখানে দিন (আগের উত্তর থেকে কপি করুন) */
-</style>
-
 <div id="bd-domain-checker">
-  <h2>ডোমেইন নাম অনুসন্ধান করুন</h2>
-  <div class="bd-form">
-    <input type="text" id="bd-domain-input" placeholder="example">
-    <select id="bd-domain-ext">
-      <option value=".com.bd">.com.bd</option>
-      <option value=".edu.bd">.edu.bd</option>
-      <option value=".gov.bd">.gov.bd</option>
-      <option value=".net.bd">.net.bd</option>
-      <option value=".org.bd">.org.bd</option>
-      <option value=".ac.bd">.ac.bd</option>
-      <option value=".mail.bd">.mail.bd</option>
-      <option value=".info.bd">.info.bd</option>
-      <option value=".বাংলা">.বাংলা</option>
-    </select>
-    <button id="bd-domain-submit">অনুসন্ধান করুন</button>
-  </div>
-  <div id="bd-domain-result"></div>
+    <h2>বাংলাদেশ ডোমেইন অ্যাভেলেবিলিটি চেকার</h2>
+
+    <div id="bd-domain-form">
+        <input type="text" id="bd-domain-input" placeholder="ডোমেইন নাম লিখুন (উদাহরণ: example)">
+        
+        <select id="bd-domain-ext">
+            <option value=".com.bd">.com.bd</option>
+            <option value=".net.bd">.net.bd</option>
+            <option value=".org.bd">.org.bd</option>
+            <option value=".edu.bd">.edu.bd</option>
+            <option value=".gov.bd">.gov.bd</option>
+            <option value=".ac.bd">.ac.bd</option>
+            <option value=".mil.bd">.mil.bd</option>
+            <option value=".info.bd">.info.bd</option>
+            <option value=".mail.bd">.mail.bd</option>
+            <option value=".বাংলা">.বাংলা</option>
+        </select>
+
+        <button id="bd-domain-submit">অনুসন্ধান করুন</button>
+    </div>
+
+    <div id="bd-domain-result"></div>
+
+    <div id="bd-domain-others"></div>
 </div>
